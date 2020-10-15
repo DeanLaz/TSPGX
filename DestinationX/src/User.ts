db2c13242c7d396d5b28045539c2ce5c041d17f8
DeanLaz <DeanLazx@gmail.com>
2020-10-15 13:12:36 -0700

    DestinationX Initial Commit

---
+++ DestinationX/src/User.ts
@@ -0,0 +1,21 @@
+import faker from "faker";
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  }
+
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.latitude())
+    }
+    // this.location.lat = Number(faker.address.latitude());
+    // this.location.lng = Number(faker.address.longitude());
+  }
+}
+
