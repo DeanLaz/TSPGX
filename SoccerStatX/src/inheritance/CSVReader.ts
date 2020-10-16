dd4aa79d179bb826d9e18abb976c9aaa9c72b86e
DeanLaz <DeanLazx@gmail.com>
2020-10-16 16:55:44 -0700

    Inheritance

---
+++ SoccerStatX/src/inheritance/CSVReader.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import { MatchResult } from "./MatchResult";
+import { MatchReader } from "./MatchReader";
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+export abstract class CSVReader<T> {
+  data: T[] = [];
+  constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T;
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: "utf-8",
+      })
+      .split("\n")
+      .map((row: string): string[] => {
+        return row.split(",");
+      })
+      .map(this.mapRow);
+  }
+}
