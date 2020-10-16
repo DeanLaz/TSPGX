df212bbf0ad55e7ca31c4a9d3d8a637a38fcb6fb
DeanLaz <DeanLazx@gmail.com>
2020-10-15 21:13:18 -0700

    Enumeration

--- SoccerStatX/src/index.ts
+++ SoccerStatX/src/index.ts
@@ -1,22 +1,30 @@
 import fs from "fs";
+import { CSVReader } from "./CSVReader";
 
-const matches = fs
-  .readFileSync("./football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CSVReader("./football.csv");
+reader.read();
+// enu, // ENUMERATION
+// ENUMS ARE BASICALLY OBJECTS THAT HAVE CLOSELY RELATED VALUES
+
+enum matchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+// const MATCH_RESULT = {
+//   HomeWin: "H",
+//   AwayWin: "A",
+//   Draw: "D",
+// };
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === matchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === matchResult.AwayWin) {
     manUnitedWins++;
   }
 }
 
-console.log(matches);
+console.log(`Man United won ${manUnitedWins} games`);
