204fbed8bb9efc3f7aa09beb86693b277f46c78f
DeanLaz <DeanLazx@gmail.com>
2020-10-20 16:02:22 -0700

    Refactors

--- SoccerStatX/src/index.ts
+++ SoccerStatX/src/index.ts
@@ -1,17 +1,16 @@
 import { CSVReader } from "./CSVReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 const csvReader = new CSVReader("./football.csv");
 
 const matchReader = new MatchReader(csvReader);
 matchReader.load();
 
-// const dateOfFirstMatch = reader.data[0][0];
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
 
-// console.log(reader.data);
-
-// enum matchResult { // enum or ENUMERATION == ENUMS ARE BASICALLY OBJECTS THAT HAVE CLOSELY RELATED VALUES
-//   HomeWin = "H",
-//   AwayWin = "A",
-//   Draw = "D",
-// }
+summary.buildAndPrintReport(matchReader.matches);
