323ff9ae72711d5e2d339ea3c40e079373efe1d1
DeanLaz <DeanLazx@gmail.com>
2020-10-19 18:01:26 -0700

    Problems Fix

--- SoccerStatX/src/inheritance/CSVReader.ts
+++ SoccerStatX/src/inheritance/CSVReader.ts
@@ -1,19 +1,19 @@
-import fs from "fs";
-import { MatchReader } from "./MatchReader";
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export abstract class CSVReader<T> {
-  data: T[] = [];
-  constructor(public filename: string) {}
-  abstract mapRow(row: string[]): T;
-  read(): void {
-    this.data = fs
-      .readFileSync(this.filename, {
-        encoding: "utf-8",
-      })
-      .split("\n")
-      .map((row: string): string[] => {
-        return row.split(",");
-      })
-      .map(this.mapRow);
-  }
-}
+// import fs from "fs";
+// import { MatchReader } from "./MatchReader";
+// type MatchData = [Date, string, string, number, number, MatchResult, string];
+// export abstract class CSVReader<T> {
+//   data: T[] = [];
+//   constructor(public filename: string) {}
+//   abstract mapRow(row: string[]): T;
+//   read(): void {
+//     this.data = fs
+//       .readFileSync(this.filename, {
+//         encoding: "utf-8",
+//       })
+//       .split("\n")
+//       .map((row: string): string[] => {
+//         return row.split(",");
+//       })
+//       .map(this.mapRow);
+//   }
+// }
