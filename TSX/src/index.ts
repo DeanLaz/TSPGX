89908495625d5aab5c8564a261bbcc1b48be9fe0
DeanLaz <DeanLazx@gmail.com>
2020-10-20 20:57:02 -0700

    User

--- TSX/src/index.ts
+++ TSX/src/index.ts
@@ -1,1 +1,10 @@
+import { User } from "./models/User";
+
 console.log("Dean");
+
+const user = new User({ name: "Dean", age: 23 });
+
+user.set({ name: "newname", age: 9999 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
