c6e41379dfdae04627d457bd4db90807a217a887
DeanLaz <DeanLazx@gmail.com>
2020-10-21 00:31:04 -0700

    User + Node + DB + Axios

--- TSX/src/index.ts
+++ TSX/src/index.ts
@@ -1,10 +1,5 @@
 import { User } from "./models/User";
 
-console.log("Dean");
+const user = new User({ id: 1 });
 
-const user = new User({ name: "Dean", age: 23 });
-
-user.set({ name: "newname", age: 9999 });
-
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.fetch();
--- TSX/src/models/User.ts
+++ TSX/src/models/User.ts
@@ -1,8 +1,11 @@
+import axios, { AxiosResponse } from "axios";
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
-type Callback = () => {};
+type Callback = () => void;
 export class User {
   events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
@@ -14,7 +17,26 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback) {}
-}
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || []; // BUG: this.events[eventName] === Callback[] or undefined...
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
 
-new User({ name: "asdf", age: 20 });
+    if (!handlers || handlers.length) {
+      return;
+    }
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+}
