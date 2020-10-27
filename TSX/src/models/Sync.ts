d53afd13fe1a32b22b770f3ec4905c372a062aec
DeanLaz <DeanLazx@gmail.com>
2020-10-26 21:06:16 -0700

    Sync.ts + Refactors

---
+++ TSX/src/models/Sync.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosPromise } from "axios";
+import { UserProps } from "./User";
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+  save(data: UserProps): AxiosPromise {
+    const { id } = data;
+    if (id) {
+      //PUT
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      //POST
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
