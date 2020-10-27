99ccf67e74dc9f7f662cda0fbe05759f1d72da2d
DeanLaz <DeanLazx@gmail.com>
2020-10-26 23:32:26 -0700

    Attributes.ts

--- TSX/src/index.ts
+++ TSX/src/index.ts
@@ -2,7 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ id: 1 });
 
-user.set({ name: "DEANS NEW NAME", age: 2001 });
+// user.set({ name: "DEANS NEW NAME", age: 2001 });
 
 user.events.on("change", () => {
   console.log("change");
