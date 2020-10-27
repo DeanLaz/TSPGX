9479531810b69107522e42756ba2c76fcbe252a7
DeanLaz <DeanLazx@gmail.com>
2020-10-27 00:51:04 -0700

    Model.ts+ implemented Inheritance, extended User

--- TSX/src/index.ts
+++ TSX/src/index.ts
@@ -1,10 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1, name: "DEANS NEWEST NAME" });
+// const user = new User({ id: 1, name: "DEANS NEWEST NAME" });
 
-const { on, trigger, fetch, save } = user;
-on("change", () => {
-  console.log(user);
-});
-save();
-fetch();
+// const { on, trigger, fetch, save } = user;
+// on("change", () => {
+//   console.log(user);
+// });
+// s
